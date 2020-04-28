abdd4f0a85ac586dbfc8a8522507d8303796ebdf
Bence <borsi92@gmail.com>
2020-04-28 21:55:53 +0200

    first element

--- static/js/script.js
+++ static/js/script.js
@@ -1,32 +1,65 @@
-let disks = document.querySelectorAll('.disk');
+// let disks = document.querySelectorAll('.disk');
+let disks = [];
 let dropZones = document.querySelectorAll('.tower');
 
-disks.forEach(disk => {
+selectFirstDisk();
+addListener();
 
-    disk.addEventListener('dragstart', event => {
-        console.log('dragstart');
-        event.dataTransfer.setData('text', event.target.id);
+function selectFirstDisk (){
+    disks = [];
+    for (let i = 0;i<dropZones.length; i++){
+        let zone = dropZones[i];
+        if (zone.firstElementChild!==null){
+            disks.push(zone.firstElementChild);
+        }
+    }
+}
+
+function removeListener (){
+    disks.forEach(disk => {
+        disk.removeEventListener('dragstart', event => {
+            event.dataTransfer.clearData();
+
+        });
+
+        // disk.addEventListener('dragend', event => {
+        //     disk.classList.remove('dragstart');
+        // });
+
     });
+}
 
-    // disk.addEventListener('dragend', event => {
-    //     disk.classList.remove('dragstart');
-    // });
+function addListener(){
+    disks.forEach(disk => {
+        disk.addEventListener('dragstart', event => {
+            // console.log('dragstart');
+            event.dataTransfer.setData('text', event.target.id);
+        });
 
-});
+        // disk.addEventListener('dragend', event => {
+        //     disk.classList.remove('dragstart');
+        // });
+
+    });
+}
 
 dropZones.forEach(dropZone => {
 
     dropZone.addEventListener('dragover', event => {
         event.preventDefault();
-        console.log('dragover')
+        // console.log('dragover')
     });
 
 
     dropZone.addEventListener('drop', event => {
         event.preventDefault();
         let data = event.dataTransfer.getData("text");
+        event.dataTransfer.clearData("text");
         let list = event.target;
         list.insertBefore(document.getElementById(data), list.childNodes[0]);
         console.log(list.childNodes);
+        removeListener();
+        selectFirstDisk();
+        addListener()
     });
 });
