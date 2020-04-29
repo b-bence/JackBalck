b644fca761b6087e4dc365253090f6faf7c5d713
Tibor Düsüngülü <dusungulu.tibi@gmail.com>
2020-04-29 17:42:38 +0200

    Rule is now working as HELL, literally hell

--- static/js/script.js
+++ static/js/script.js
@@ -43,34 +43,47 @@ dropZones.forEach(dropZone => {
 
     dropZone.addEventListener('drop', event => {
         event.preventDefault();
+
         let data = event.dataTransfer.getData("text");
         let list = event.target;
         let sameDisk;
 
-        sameDisk = list.children[0] === document.getElementById(data) ;
+
+        sameDisk = list.children[0] === document.getElementById(data);
 
         if (event.target.getAttribute('class') === 'tower' && !sameDisk) {
             try {
-                if (event.target.childNodes.length === 1){
+                // console.log(event.target.childNodes)
+                if (event.target.childNodes.length === 1) {
                     list.insertBefore(document.getElementById(data), list.childNodes[0]);
                     let counter = document.getElementById('counter');
                     counter.innerHTML = parseInt(counter.innerHTML) + 1;
                 }
                 else {
-                    let towerIndex = parseInt(event.target.getAttribute('data'))
-                    let towerDisk = parseInt(disks[towerIndex].getAttribute('index'))
-                    let currentDisk = data.split('')[data.length - 1]
-                    if (currentDisk < towerDisk){
-                        list.insertBefore(document.getElementById(data), list.childNodes[0]);
-                        let counter = document.getElementById('counter');
-                        counter.innerHTML = parseInt(counter.innerHTML) + 1;
+                    let currentDisk = parseInt(data.split('')[data.length - 1])
+                    let firstTowerDiskSize;
+
+                    labelLoop:
+                    for (let node of event.target.childNodes) {
+                        try {
+                            if (node.hasAttribute('draggable')) {
+                                firstTowerDiskSize = parseInt(node.getAttribute('index'))
+                                console.log(firstTowerDiskSize)
+                                if (firstTowerDiskSize > currentDisk && firstTowerDiskSize)
+                                    list.insertBefore(document.getElementById(data), list.childNodes[0]);
+                                break labelLoop;
+                            }
+                        } catch (e) {
+                            if (firstTowerDiskSize === undefined)
+                                list.insertBefore(document.getElementById(data), list.childNodes[0]);
+                        }
                     }
                 }
             } catch (e) {
                 console.log('Something went wrong!')
             }
-
         }
+        console.log('----------');
         removeListener();
         selectFirstDisk();
         addListener();
