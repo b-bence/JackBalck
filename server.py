29f044e703ef3f40ca1d763221d625d2ecf186c6
Telbit <bendeguz.telegdi@gmail.com>
2020-04-30 00:17:46 +0200

    prezi

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-disk_count = 0
+disk_count = 6
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -11,12 +11,29 @@ def home():
         global disk_count
         disk_count = int(request.form['disk-count'])
         return redirect('/game')
-    return render_template('home.html')
+    #
+    return render_template("home.html")
 
 
 @app.route('/game')
 def game():
     return render_template('game.html', disk_count=disk_count)
+
+
+@app.route('/prosAndCons')
+def pros_and_cons():
+    pros_cons = ['teamwork', 'communication', 'choosing game', 'task allocation', 'new technologies']
+    return render_template("pros-cons.html", pros_cons=pros_cons)
+
+
+@app.route('/story')
+def story():
+    return render_template('story.html')
+
+
+@app.route('/week')
+def week():
+    return render_template('week.html')
 
 
 if __name__ == '__main__':
